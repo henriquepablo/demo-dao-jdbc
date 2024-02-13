90b6de4a6ca40d14bca3e64881f1b8d87ba3a244
henriquepablo <pablo269704@gmail.com>
2024-02-13 11:55:40 -0300

    :sparkles: feat: Implementation of search seller by id

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "books");
 		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		SellerDao sellerDao = new DaoFactory().createSellerDao();
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,12 +1,25 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
-
+	
+	private Connection conn; 
+	
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
@@ -27,8 +40,45 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE seller.Id = ?");
+		
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			
+			// verifica se algum dado foi retornado do db
+			if (rs.next()) {
+				Department dep = new Department();
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setName(rs.getString("DepName"));
+				
+				Seller seller = new Seller();
+				seller.setId(rs.getInt("Id"));
+				seller.setName(rs.getString("Name"));
+				seller.setEmail(rs.getString("Email"));
+				seller.setBaseSalary(rs.getDouble("BaseSalary"));
+				seller.setBirthDate(rs.getDate("BirthDate"));
+				seller.setDepartment(dep);
+				
+				return seller;
+				
+			}
+			return null;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
