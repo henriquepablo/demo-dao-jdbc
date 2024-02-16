92c8af50b58dca2d47a1b2a8e3db8fa43c109b25
henriquepablo <pablo269704@gmail.com>
2024-02-16 13:48:13 -0300

    :sparkles: feat: implementation insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -34,6 +35,10 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		System.out.println("=== TEST 4: seller insert===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! new id = " + newSeller.getId());
 		
 	}
 
