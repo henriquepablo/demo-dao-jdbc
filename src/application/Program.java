f689eaa493de2303a013f9738e3f2006a82d9995
henriquepablo <pablo269704@gmail.com>
2024-02-16 13:59:25 -0300

    :sparkles: feat: implementation update

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,6 +40,12 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
 		
+		System.out.println("=== TEST 5: seller update===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		
 	}
 
 }
