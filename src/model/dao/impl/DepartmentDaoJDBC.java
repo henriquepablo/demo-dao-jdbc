43d7c502811990de31e65c651eb0a171ed52b245
henriquepablo <pablo269704@gmail.com>
2024-02-17 01:17:50 -0300

    :sparkles: feat: Creating implementation DepartmentDaoJDBC

---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+	@Override
+	public void insert(Department obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Department obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Department> fidAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
